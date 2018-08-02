5cbc65cf3146ba8b06ddbcedba3f0b713fc77d5e
sean <srowla94@gmail.com>
2018-08-02 01:11:09 -0400

    started python

---
+++ Python/practice/cond.py
@@ -0,0 +1,10 @@
+print("please enter your name")
+name = input()
+
+if name == "jon":
+    print("hi jon")
+elif name == "sean":
+    print("hi sean")
+else:
+    print("who are you?")
+
---
+++ Python/practice/mileage.py
@@ -0,0 +1,6 @@
+print("how may kilometers did you run today?")
+kms = float ( input() )
+mile = kms / 1.60934
+print(f"you ran {kms} k/m")
+print(f"you ran {round(mile, 2)} miles")
+
